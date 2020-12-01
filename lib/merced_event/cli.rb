adb3a9bbe1e0f18b70b3cbdf9ee9673ada6fbcff
Christerle Nounoute <christerlesn@gmail.com>
2020-12-01 18:39:38 +0000

    modified cli.rb #list_titles to data not hard code

--- lib/merced_event/cli.rb
+++ lib/merced_event/cli.rb
@@ -9,10 +9,9 @@ class MercedEvent::CLI
   def list_titles
     @events = MercedEvent::Event.current
     puts "Upcoming events:"
-    puts <<-DOC
-    1. UC MERCED UPSTART PRESENTS A FREE ONLINE CONCERT WITH SALLYSWAG
-    2. AMA WITH REBECCA RYALS, PHD. WORLD SOIL DAY IS 12/5. IN CELEBRATION, PLEASE JOIN US ON MONDAY, 12/7 FOR A SPECIAL AMA WITH SOIL HEALTH EXPERT, REBECCA RYALS, PHD!
-    DOC
+    @events.each.with_index(1) do |event, n|
+      puts "#{n}. #{event.title}"
+    end
   end
   
   def all_events
@@ -24,11 +23,14 @@ class MercedEvent::CLI
   def menu
     input = nil 
     while input != "exit"
-        puts "For more info, please enter the number on the list corresponding to the event. To see all, type 'all'. To exit, type 'exit'."
+        puts "Please enter the number on the list corresponding to the event to see more. To see all, type 'all'. To exit, type 'exit'."
         input = gets.strip.downcase
         if input.to_i > 0
           event_detail = @events[input.to_i-1]
-          puts "TITLE:#{event_detail.title} - DATE: #{event_detail.date} - #{event_detail.time} - For more information, go to: #{event_detail.url}"
+          puts "TITLE:#{event_detail.title}
+          DATE: #{event_detail.date} 
+          #{event_detail.time}
+          For more information, go to: #{event_detail.url}"
         elsif input == "all"
           all_events
         else
