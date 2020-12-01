84ad7ee40c52164633c957842dbe9b1099bd42bf
Christerle Nounoute <christerlesn@gmail.com>
2020-12-01 05:59:42 +0000

    fixed error for menu individual input selection

--- lib/merced_event/cli.rb
+++ lib/merced_event/cli.rb
@@ -7,6 +7,7 @@ class MercedEvent::CLI
   end
   
   def list_titles
+    @events = MercedEvent::Event.current
     puts "Upcoming events:"
     puts <<-DOC
     1. UC MERCED UPSTART PRESENTS A FREE ONLINE CONCERT WITH SALLYSWAG
@@ -15,7 +16,6 @@ class MercedEvent::CLI
   end
   
   def all_events
-    @events = MercedEvent::Event.current
     @events.each.with_index(1) do |event, n|
       puts " #{n}. TITLE:#{event.title} - DATE: #{event.date} - #{event.time} - For more information, go to: #{event.url}"
     end
@@ -27,7 +27,7 @@ class MercedEvent::CLI
         puts "For more info, please enter the number on the list corresponding to the event. To see all, type 'all'. To exit, type 'exit'."
         input = gets.strip.downcase
         if input.to_i > 0
-          event_detail = @events[input.to_i - 1]
+          event_detail = @events[input.to_i-1]
           puts "TITLE:#{event_detail.title} - DATE: #{event_detail.date} - #{event_detail.time} - For more information, go to: #{event_detail.url}"
         elsif input == "all"
           all_events
@@ -36,20 +36,6 @@ class MercedEvent::CLI
         end
       end
     end
-        
-        
-    #     if input == "all"
-    #       puts all_events
-    #     elsif input.to_i == 1
-    #       puts "   1. UC MERCED UPSTART PRESENTS A FREE ONLINE CONCERT WITH SALLYSWAG. Date: MONDAY, DECEMBER 7, 2020. Time: 12:00 PM - 1:00 PM "
-    #     elsif input.to_i == 2 
-    #       puts "AMA WITH REBECCA RYALS, PHD. WORLD SOIL DAY IS 12/5. IN CELEBRATION, PLEASE JOIN US ON MONDAY, 12/7 FOR A SPECIAL AMA WITH SOIL HEALTH EXPERT, REBECCA RYALS, PHD! 
-    #   Date: MONDAY, DECEMBER 7, 2020. Time: 2:00 PM - 3:00 PM"
-    #     else
-    #       puts "Unclear data provided. Please try again."
-    #     end
-    #   end
-    # end
     
     def goodbye
       puts "Thanks for visiting!"
